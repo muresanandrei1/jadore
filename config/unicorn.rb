27e821694d25e60f04bf23303c76d4c01dc885bd
Muresan Andrei <muresan.1andrei@gmail.com>
2017-09-06 02:22:07 +0300

    add unicorn.rb

---
+++ config/unicorn.rb
@@ -0,0 +1,16 @@
+deploy_to = "/home/deployer/jadore"
+root = "#{deploy_to}/current"
+working_directory root
+pid "#{deploy_to}/shared/pids/unicorn.pid"
+stderr_path "#{deploy_to}/shared/log/unicorn.log"
+stdout_path "#{deploy_to}/shared/log/unicorn.log"
+
+listen "/#{deploy_to}/shared/sockets/unicorn.demo-app.sock"
+worker_processes 1
+preload_app true
+timeout 30
+
+# Force the bundler gemfile environment variable to
+before_exec do |_|
+ ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
