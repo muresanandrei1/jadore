6f1bd477de46884ca97cb9bed912bd0fc59a3ba8
Muresan Andrei <muresan.1andrei@gmail.com>
2017-09-06 17:21:41 +0300

    add jadore socekt

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@ pid "#{deploy_to}/shared/pids/unicorn.pid"
 stderr_path "#{deploy_to}/shared/log/unicorn.log"
 stdout_path "#{deploy_to}/shared/log/unicorn.log"
 
-listen "/#{deploy_to}/shared/sockets/unicorn.demo-app.sock"
+listen "/#{deploy_to}/shared/sockets/unicorn.jadore.sock"
 worker_processes 1
 preload_app true
 timeout 30
