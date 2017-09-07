c5fa240f5b0b8617b80f065f08a445fa78cd8831
Muresan Andrei <muresan.1andrei@gmail.com>
2017-09-07 16:07:41 +0300

    add contacts page and menu items

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,9 @@ Rails.application.routes.draw do
     post 'destroy_pictures', on: :collection
   end
 
+  match '/contacts', to: 'contacts#new', via: 'get'
+  
+  resources "contacts", only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
