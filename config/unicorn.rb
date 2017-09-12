c43bc8c9754a1547b854f0a290bd6c346a42abd2
Muresan Andrei <muresan.1andrei@gmail.com>
2017-09-12 22:58:17 +0300

    change socket location

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@ pid "#{deploy_to}/shared/pids/unicorn.pid"
 stderr_path "#{deploy_to}/shared/log/unicorn.log"
 stdout_path "#{deploy_to}/shared/log/unicorn.log"
 
-listen "/#{deploy_to}/shared/sockets/unicorn.jadore.sock"
+listen "/tmp/unicorn.jadore.sock"
 worker_processes 1
 preload_app true
 timeout 30
